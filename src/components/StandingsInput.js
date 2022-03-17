02e5292e1264fb159979f83f2bf1508ed9a49a15
rayvaldez <ray_valdez@hotmail.co.uk>
2022-03-17 23:51:57 +0000

    Create input form component for standings

---
+++ src/components/StandingsInput.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class StandingsInput extends React.Component {
+
+  state = {
+    input: ''
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('fetching')
+    this.props.fetchManagers(this.state.input)
+    this.setState({
+      input: ''
+    });
+  };
+
+  render() {
+    return(
+      <div className="inputManager">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="user" value={this.state.input} onChange={this.handleChange}/>
+          <input type="submit" value="submit"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default StandingsInput;
