1dd3cf504eb0046f4818371d80f7b85a6d44ba54
rayvaldez <ray_valdez@hotmail.co.uk>
2022-03-17 23:53:55 +0000

    Remove standings logic from App component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React from 'react';
 import './App.css';
-import Standings from './components/Standings';
+import StandingsContainer from './containers/StandingsContainer';
 
 class App extends React.Component {
-
-  state = {
-    user: {},
-    input: ''
-  };
-
-  handleChange = (event) => {
-    this.setState({
-      input: event.target.value
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    console.log('fetching')
-    fetch(`https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/leagues-classic/${this.state.input}/standings/`)
-      .then(res => (res.ok ? res : Promise.reject(res)))
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ user: data });
-      });
-  }
 
   render() {
     return (
       <div className="App">
         <h1>FPL Data</h1>
-        <div className="inputManager">
-          <form onSubmit={this.handleSubmit}>
-            <input type="text" name="user" onChange={this.handleChange}/>
-            <input type="submit" value="submit"/>
-          </form>
-        </div>
-        <Standings managers={this.state.user.standings}/>
+        <StandingsContainer />
       </div>
     );
   }
