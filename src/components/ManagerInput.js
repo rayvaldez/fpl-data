ee1b99abade8d2e73a415ceb44302251bda84c95
rayvaldez <ray_valdez@hotmail.co.uk>
2022-03-19 10:48:45 +0000

    Create input field for Manager

---
+++ src/components/ManagerInput.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class ManagerInput extends React.Component {
+
+  state = {
+    input: ''
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('fetching')
+    this.props.fetchManager(this.state.input)
+    this.setState({
+      input: ''
+    });
+  };
+
+  render() {
+    return (
+      <div className="inputManager">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="user" value={this.state.input} onChange={this.handleChange}/>
+          <input type="submit" value="submit"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ManagerInput;
