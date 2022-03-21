1dadea8e0bb5715a4dacb4d622b685c7873a4b50
rayvaldez <ray_valdez@hotmail.co.uk>
2022-03-21 09:58:13 +0000

    Add difference in total from 1st place

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 const Standings = (props) => {
 
+  const leaderScore = props.managers.results[0].total
+
   return (
     <div className="standings">
       {props.managers && props.managers.results.map(user => {
         return (
           <div key={user.id}>
             <h5>{user.rank_sort}.{user.player_name} - {user.entry_name} - Total Points - {user.total}</h5>
+            {leaderScore - user.total > 0 ? <h6>Points from 1st {leaderScore - user.total}</h6> : null}
           </div>
         )
       })}
