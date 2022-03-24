6ed184bb2a393dffa3f4cfc5d64c78ce284926d5
rayvaldez <ray_valdez@hotmail.co.uk>
2022-03-24 21:07:57 +0000

    Add fetchManager action to managersReducer

--- src/reducers/managerReducer.js
+++ src/reducers/managerReducer.js
@@ -1,10 +1,8 @@
-const initialState = {
-  manager: []
-}
-
-export default function managerReducer(state = initialState, action) {
+export default function managersReducer(state = {managers: []}, action) {
 
   switch (action.type) {
+    case 'FETCH_MANAGERS':
+      return {managers: action.payload}
     default:
       return state;
   }
