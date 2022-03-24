b176be4a2521e2fbe06762e5578b63d99702b6d0
rayvaldez <ray_valdez@hotmail.co.uk>
2022-03-24 21:00:04 +0000

    Remove componentDidMount

--- src/containers/StandingsContainer.js
+++ src/containers/StandingsContainer.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import Standings from '../components/Standings';
 import StandingsInput from '../components/StandingsInput';
+import { fetchLeague } from '../actions/fetchLeague';
+import { connect } from 'react-redux';
 
 class StandingsContainer extends React.Component {
-
-  state = {
-    managers: {}
-  };
-
-  fetchLeague = (leagueId) => {
-    fetch(`https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/leagues-classic/${leagueId}/standings/`)
-      .then(res => (res.ok ? res : Promise.reject(res)))
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ managers: data });
-      });
-  }
 
   render() {
     return(
       <div>
-        <StandingsInput fetchLeague={this.fetchLeague}/>
-        <Standings managers={this.state.managers.standings}/>
+        <StandingsInput fetchLeague={this.props.fetchLeague}/>
+        <Standings managers={this.props.managers.standings}/>
       </div>
     )
   }
-
 }
 
-export default StandingsContainer;
+const mapStateToProps = state => {
+  return {
+    managers: state.managers
+  }
+}
+
+export default connect(mapStateToProps, {fetchLeague})(StandingsContainer)
