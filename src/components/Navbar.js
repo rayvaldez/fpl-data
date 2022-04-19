22196a1efa2d49dd65c39ced60458e37f8769ad1
rayvaldez <ray_valdez@hotmail.co.uk>
2022-04-19 21:34:34 +0100

    Format links into unordered list

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import StandingsContainer from '../containers/StandingsContainer';
+import ManagerContainer from '../containers/ManagerContainer';
+import App from '../App';
 
 const Navbar = () => {
 
   return (
     <div className="navbar">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/standings">Standings</Link>
-        <Link to="/manager">Manager</Link>
-      </nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/standings">Standings</Link>
+        </li>
+        <li>
+          <Link to="/manager">Manager</Link>
+        </li>
+      </ul>
     </div>
   )
 }
