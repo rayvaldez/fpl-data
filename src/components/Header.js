994e733187ba6c28c9ed17aa232e03bca48bc8fa
rayvaldez <ray_valdez@hotmail.co.uk>
2022-05-31 15:51:39 +0100

    Format Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import { ThemeProvider } from '@mui/material/styles';
+
 
 const Header = () => {
+  let theme = createTheme();
+  theme = responsiveFontSizes(theme);
 
   return (
-    <div className="header">
-      <h1>FPL Data</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Typography variant="h4" align="center">FPL Data</Typography>
+    </ThemeProvider>
   )
 }
 
