fbef7395e023ce51138879bb49b0c449ff607932
rayvaldez <ray_valdez@hotmail.co.uk>
2022-07-11 19:41:39 +0100

    Import Mui components for Manager.js

--- src/components/Manager.js
+++ src/components/Manager.js
@@ -1,10 +1,20 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import React from 'react';
 
 const Manager = (props) => {
 
   return (
-    <div className="manager">
-      <h3>{props.manager.first_name} {props.manager.last_name} {props.manager.team_name}</h3>
+    <Box className="manager">
+      <Grid item xs={7}>
+        <Typography variant="h6" sx={{
+          color: '#FAF9F6',
+          mt: '2vh'
+          }}>
+        {props.manager.first_name} {props.manager.last_name} - {props.manager.team_name}
+        </Typography>
+      </Grid>
       {props.manager && props.manager.current && props.manager.current.map(user => {
         return (
           <div key={user.event}>
@@ -12,7 +22,7 @@ const Manager = (props) => {
           </div>
         )
       })}
-    </div>
+    </Box>
   )
 }
 
