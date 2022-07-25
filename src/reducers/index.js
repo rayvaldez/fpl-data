5b2497295b4271a417ca606f0bb5792e6466d7e6
rayvaldez <ray_valdez@hotmail.co.uk>
2022-07-25 19:12:19 +0100

    Add information reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import leagueReducer from './leagueReducer';
 import managerReducer from './managerReducer';
+import informationReducer from './informationReducer';
 
 export default combineReducers({
   leagueReducer,
-  managerReducer
+  managerReducer,
+  informationReducer
 });
