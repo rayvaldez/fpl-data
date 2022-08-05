51cb18ef0851066d77c6fe4dac08cabc1e2ad17c
rayvaldez <ray_valdez@hotmail.co.uk>
2022-08-05 22:14:29 +0100

    Import SwiperJs and create carousel for teams

--- src/components/LandingInformation.js
+++ src/components/LandingInformation.js
@@ -2,13 +2,42 @@ import React from 'react';
 import { gameData } from '../gameData';
 import { Team } from './Team';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+import 'swiper/css';
+import "swiper/css/free-mode";
+import "swiper/css/pagination"
+
+import { FreeMode, Pagination } from 'swiper';
+
+ 
 const LandingInformation = () => {
 
   return (
-    <Box sx={{ m: '0vh 4vw 2vh 4vw' }}>
-     <Team team={gameData[0].teams[0]}/>
+    <Box>
+      <Typography sx={{ color: '#faf9f6', ml: '4vw', mt: '-2vh', mb: '0.4vh'}}>
+        Team Stats
+      </Typography>
+      <Box>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={-80}
+          grabCursor={true}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper"
+          >
+          {gameData[0].teams.map(teamId =>
+          <SwiperSlide key={teamId.name}>
+            <Team key={teamId.id} team={teamId} />
+          </SwiperSlide>
+          )}
+        </Swiper>        
+      </Box>
     </Box>
   )
 }
