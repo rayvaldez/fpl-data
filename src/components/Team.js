98beabd0469c182314189a6a94d93ab9535de7ae
rayvaldez <ray_valdez@hotmail.co.uk>
2022-08-05 22:18:36 +0100

    Format Team component layout

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import { Card } from '@mui/material';
+import Box from '@mui/material/Box';
 
 const addPeriod = (int) => {
   const number = int.toString().split('');
@@ -10,31 +10,29 @@ const addPeriod = (int) => {
 export const Team = (props) => {
 
   return (
-    <div>
-      {console.log(props.team)}
-      <Card variant="outlined" sx={{ backgroundColor: '#26262a', borderRadius: '10px'}}>
-        <Typography variant='h6' sx={{ 
-          color: '#faf9f6',
-          p: '1.5vh 0vw 0vh 6vw'
-        }}
-        >
-          {props.team.name}
-        </Typography>
-        <br></br>
-        <br></br>
-        <Typography sx={{ color: '#FAF9F6' }}>
-          <ul>
-            <li>Strength - {props.team.strength}</li>
-            <li>Home attack strength - {addPeriod(props.team.strength_attack_home)}</li>
-            <li>Home defence strength - {addPeriod(props.team.strength_defence_home)}</li>
-            <li>Away attack strength - {addPeriod(props.team.strength_attack_away)}</li>
-            <li>Away defence strength - {addPeriod(props.team.strength_defence_away)}</li>
-            <br></br>
-            <li>Overall home - {addPeriod(props.team.strength_overall_home)}</li>
-            <li>Overall away - {addPeriod(props.team.strength_overall_away)}</li>
-          </ul>
-        </Typography>  
-      </Card>
-    </div>
+    <Box sx={{ 
+      backgroundColor: '#26262a', 
+      borderRadius: '13px', 
+      p: '1em', 
+      ml: '15px', 
+      width: '70vw'
+      }}
+    >
+      <Typography sx={{ 
+        color: '#faf9f6'
+      }}
+      >
+        {props.team.name}
+      </Typography><br></br>
+      <Typography sx={{ color: '#FAF9F6' }}>  
+        Strength - {props.team.strength}<br></br>
+        Home attack strength - {addPeriod(props.team.strength_attack_home)}<br></br>
+        Home defence strength - {addPeriod(props.team.strength_defence_home)}<br></br>
+        Away attack strength - {addPeriod(props.team.strength_attack_away)}<br></br>
+        Away defence strength - {addPeriod(props.team.strength_defence_away)}<br></br>
+        Overall home - {addPeriod(props.team.strength_overall_home)}<br></br>
+        Overall away - {addPeriod(props.team.strength_overall_away)}<br></br>
+      </Typography>  
+    </Box>
   )
 };
