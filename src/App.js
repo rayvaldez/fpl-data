8790ded71be1df897ebfde7144d2ac4e69bd818a
rayvaldez <ray_valdez@hotmail.co.uk>
2022-08-05 22:13:19 +0100

    Create theme to initialise font

--- src/App.js
+++ src/App.js
@@ -5,23 +5,38 @@ import StandingsContainer from './containers/StandingsContainer';
 import ManagerContainer from './containers/ManagerContainer';
 import LandingContainer from './containers/LandingContainer';
 import { Routes, Route } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 
+      'Roboto'
+  }
+})
 
 class App extends React.Component {
 
   render() {
     return (
-      <Box sx={{
-        width: '100%',
-        height: '95vh',
-        backgroundColor: '#151515',
-        pt: '3vh'
-      }}>
-        <Routes>
-          <Route path="/" element={<LandingContainer />} />
-          <Route path="/standings" element={<StandingsContainer />} />
-          <Route path="/manager" element={<ManagerContainer />} />
-        </Routes>
-      </Box>
+      <ThemeProvider theme={theme}>
+        <Box sx={{
+          width: '100%',
+          height: '95vh',
+          backgroundColor: '#151515',
+          pt: '1vh'
+        }}>
+          <Routes>
+            <Route path="/fpl-data" element={<LandingContainer />} />
+            <Route path="/fpl-data/standings" element={<StandingsContainer />} />
+            <Route path="/fpl-data/manager" element={<ManagerContainer />} />
+          </Routes>
+        </Box>
+      </ThemeProvider>
     );
   }
 }
