dc699334ebe69a45e83904c4d79f1f4474435a9c
rayvaldez <ray_valdez@hotmail.co.uk>
2022-08-11 19:09:39 +0100

    Include manager team picks in fetched data

--- src/actions/fetchManager.js
+++ src/actions/fetchManager.js
@@ -1,6 +1,7 @@
 export function fetchManager(managerId) {
 
   let managerInfo = {}
+  let managerPicks = {}
   let managerHistory = {}
 
   return (dispatch) => {
@@ -9,17 +10,33 @@ export function fetchManager(managerId) {
       .then(res => res.json())
       .then(manager => {
         managerInfo = manager
-
+        
+        const fetchManagerPicks = async (managerId) => {
+          const request = await fetch(`https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/entry/${managerId}/event/1/picks/`)
+          console.log(request)
+          return request
+        }
+  
+        fetchManagerPicks(managerId)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          managerPicks = data
+        })
+          
         return fetch(`https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/entry/${managerId}/history/`)
       }).then(res => (res.ok ? res : Promise.reject(res)))
         .then(res => res.json())
         .then(data => {
           managerHistory = data
 
+          managerHistory.picks = managerPicks
+          managerHistory.info = managerInfo
           managerHistory.first_name = managerInfo.player_first_name
           managerHistory.last_name = managerInfo.player_last_name
           managerHistory.team_name = managerInfo.name
-        }).then(() => dispatch({
+        })
+        .then(() => dispatch({
           type: 'FETCH_MANAGER',
           payload: managerHistory
         }));
