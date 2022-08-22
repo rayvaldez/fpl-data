5e0267fcafb9028ceb606e84007c26756e8e7bae
rayvaldez <ray_valdez@hotmail.co.uk>
2022-08-22 13:48:18 +0100

    Edit fetch call to take 2 calls

--- src/actions/fetchInformation.js
+++ src/actions/fetchInformation.js
@@ -1,12 +1,34 @@
 export function fetchInformation() {
+
+  let information = {}
+
+  let api1 = 'https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/bootstrap-static/';
+  let api2 = 'https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/fixtures/';
   
   return (dispatch) => {
-    fetch(`https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/bootstrap-static/`)
+
+    let promise1 = fetch(api1)
     .then(res => (res.ok ? res : Promise.reject(res)))
     .then(res => res.json())
-    .then(data => dispatch({
+    .then(data => {
+      console.log(data)
+      information.general = data
+      return data
+    });
+
+    let promise2 = fetch(api2)
+    .then(res => (res.ok ? res : Promise.reject(res)))
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      information.fixtures = data
+      return data
+    });
+
+    Promise.all([promise1, promise2])
+    .then(() => dispatch({
       type: 'FETCH_INFORMATION',
-      payload: data
-    }))
-  }
+      payload: information
+    }));
+  };
 }
