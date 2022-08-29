85f8df5dc3f564c10c9ea55329cbe3f39d360f95
rayvaldez <ray_valdez@hotmail.co.uk>
2022-08-29 11:17:42 +0100

    Remove div wrapper and bring navigation to the front

--- src/components/PhoneNav.js
+++ src/components/PhoneNav.js
@@ -11,52 +11,52 @@ const PhoneNav = () => {
 
   const [value, setValue] = useState(0)
   return (
-    <div>
-      <Paper elevation={3} sx={{ 
-        position: 'fixed', 
-        bottom: 0, 
-        left: 0, 
-        right: 0 
-      }}>
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-          sx={{
-            backgroundColor: '#151515 !important',
-            '& .Mui-selected': {
-              '& .MuiBottomNavigationAction-label': {
-                color: '#39FF14'
-              }
-            },
+    <Paper elevation={3} sx={{ 
+      position: 'fixed', 
+      bottom: 0, 
+      left: 0, 
+      right: 0,
+      zIndex: '10' 
+    }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        sx={{
+          backgroundColor: '#151515 !important',
+          '& .Mui-selected': {
             '& .MuiBottomNavigationAction-label': {
-              color: '#FAF9F6'
-            }            
-          }}
-        >
-          <BottomNavigationAction
-            component={Link}
-            to="/fpl-data"
-            label="Home"
-            value="home"
-            icon={<HomeRoundedIcon sx={{ color: '#39FF14', opacity: '70%'}}/>} />
-          <BottomNavigationAction
-            component={Link}
-            to="/fpl-data/manager"
-            label="Manager"
-            value="manager"
-            icon={<PersonOutlineRoundedIcon sx={{ color: '#39FF14', opacity: '70%'}}/>} />
-          <BottomNavigationAction
-            component={Link}
-            to="/fpl-data/standings"
-            label="Standings"
-            value="standings"
-            icon={<GroupsRoundedIcon sx={{ color: '#39FF14', opacity: '70%'}}/>} />
-        </BottomNavigation>
-      </Paper>
-    </div>
+              color: '#39FF14'
+            }
+          },
+          '& .MuiBottomNavigationAction-label': {
+            color: '#FAF9F6'
+          }            
+        }}
+      >
+        <BottomNavigationAction
+          component={Link}
+          to="/fpl-data"
+          label="Home"
+          value="home"
+          icon={<HomeRoundedIcon sx={{ color: '#39FF14', opacity: '70%'}}/>} />
+        <BottomNavigationAction
+          component={Link}
+          to="/fpl-data/manager"
+          label="Manager"
+          value="manager"
+          icon={<PersonOutlineRoundedIcon sx={{ color: '#39FF14', opacity: '70%'}}/>} />
+        <BottomNavigationAction
+          component={Link}
+          to="/fpl-data/standings"
+          label="Standings"
+          value="standings"
+          icon={<GroupsRoundedIcon sx={{ color: '#39FF14', opacity: '70%'}}/>} />
+      </BottomNavigation>
+    </Paper>
+
   )
 }
 
