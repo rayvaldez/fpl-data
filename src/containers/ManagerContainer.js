f060491f67c9858cfd5372657382c62a4d710b8a
rayvaldez <ray_valdez@hotmail.co.uk>
2022-08-29 11:14:50 +0100

    Change divs to Mui Boxes and set minimum background height

--- src/containers/ManagerContainer.js
+++ src/containers/ManagerContainer.js
@@ -4,15 +4,19 @@ import Manager from '../components/Manager';
 import ManagerGraph from '../components/ManagerGraph';
 import { fetchManager } from '../actions/fetchManager';
 import { connect } from 'react-redux';
+import Box from '@mui/material/Box';
 
 class ManagerContainer extends React.Component {
 
   render() {
     return (
-      <div className="managerContainer">
+      <Box sx={{
+        minHeight: '100vh',
+        backgroundColor: '#151515'
+      }}>
         <ManagerInput fetchManager={this.props.fetchManager}/>
         <Manager manager={this.props.manager}/>
-      </div>
+      </Box>
     )
   }
 }
