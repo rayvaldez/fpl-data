3dd1fb455813fd4cf35bed616b8ddf4757382529
rayvaldez <ray_valdez@hotmail.co.uk>
2022-08-30 20:48:53 +0100

    Create component to either show fixture time or result

---
+++ src/components/FixtureResults.js
@@ -0,0 +1,87 @@
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import React from 'react';
+import teamJSON from './jsonData/teamJSON'
+
+
+const FixtureResults = (props) => {
+
+  let homeTeam = '';
+  let awayTeam = '';
+  let homeScore = 0;
+  let awayScore = 0;
+
+  // eslint-disable-next-line no-unused-vars
+  const getHomeTeam = teamJSON && teamJSON.forEach(element => {
+    if (element.id === props?.fixtures?.team_h) 
+      homeTeam = element.name
+  })
+
+  // eslint-disable-next-line no-unused-vars
+  const getAwayTeam = teamJSON && teamJSON.forEach(element => {
+    if (element.id === props.fixtures?.team_a) 
+      awayTeam = element.name
+  })
+
+  let getScore = () => {
+    if (props.fixtures?.stats.length) {
+      
+      props.fixtures.stats[0].a.forEach(element => {
+        awayScore = awayScore + element.value
+      })
+
+      props.fixtures.stats[0].h.forEach(element => {
+        homeScore = homeScore + element.value
+      })
+
+      props.fixtures.stats[2].a.forEach(element => {
+        homeScore = homeScore +element.value
+      })
+
+      props.fixtures.stats[2].h.forEach(element => {
+        awayScore = awayScore +element.value
+      })
+    }
+  }
+
+  getScore()
+  
+  const time = new Date(props.fixtures?.kickoff_time).toLocaleTimeString('en-GB',
+  { timeStyle: 'short', hour12: false, timeZone: 'Europe/London'});
+  
+  const timeOrResult = () => {
+    if (props.fixtures?.stats.length) {
+      return (
+        <Typography variant='subtitle2'>
+          {homeScore} - {awayScore}
+        </Typography>
+      )
+    } else {
+      return (
+        <Typography variant='caption' sx={{ border: '1px solid', padding: '2px' }}>
+          {time}
+        </Typography>        
+      )
+    }
+  }
+
+  return (
+    <Grid container spacing={3} sx={{
+      color: '#FAF9F6',
+      textAlign: 'center'
+    }}
+    >
+      <Grid item xs sx={{ textAlign: 'right' }}>
+        {homeTeam}
+      </Grid>
+      <Grid item xs={2}> 
+      {timeOrResult()}
+      </Grid>
+      <Grid item xs sx={{ textAlign: 'left' }}>
+        {awayTeam}
+      </Grid>
+    </Grid>
+  )
+}
+
+export default FixtureResults;
