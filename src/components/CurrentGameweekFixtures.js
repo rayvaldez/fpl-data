bcbd2b0698363119b315748197d733b3499a67aa
rayvaldez <ray_valdez@hotmail.co.uk>
2022-09-15 17:57:28 +0100

    Add null condition to bypass gw7 postponed games

--- src/components/CurrentGameweekFixtures.js
+++ src/components/CurrentGameweekFixtures.js
@@ -9,8 +9,8 @@ import TimeVResults from './TimeVResults';
 const LandingFixtureResults = (props) => {
 
   // Find next gameweek
-  const findNextGameweek = props.fixtures ? props.fixtures.find(element => element.finished === false) : null
-
+  const findNextGameweek = props.fixtures ? props.fixtures.find(element => element.event != null && element.finished === false) : null
+ 
   // Remember to revert this code!!!
   const gameweek = props.fixtures && findNextGameweek.event
 
