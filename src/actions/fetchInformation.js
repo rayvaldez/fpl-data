2fb611365c795091db80538ff030de04aa79b277
rayvaldez <ray_valdez@hotmail.co.uk>
2022-09-29 23:10:18 +0100

    Add gameweek to dispatched data & move promise3 to dreamTeam component

--- src/actions/fetchInformation.js
+++ src/actions/fetchInformation.js
@@ -1,10 +1,11 @@
 export function fetchInformation() {
 
   let information = {}
-
+  let nextGW;
   let api1 = 'https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/bootstrap-static/';
   let api2 = 'https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/fixtures/';
-  let api3 = 'https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/dream-team/5/';
+  // api3 has the gameweek hardcoded, work on a fix!
+  // let api3 = `https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/dream-team/${nextGWID}/`;
   
   return (dispatch) => {
 
@@ -12,7 +13,9 @@ export function fetchInformation() {
     .then(res => (res.ok ? res : Promise.reject(res)))
     .then(res => res.json())
     .then(data => {
+      nextGW = data.events.find(element => element.finished === false)
       information.general = data
+      information.currentGW = nextGW.id
       return data
     });
 
@@ -23,16 +26,17 @@ export function fetchInformation() {
       information.fixtures = data
       return data
     });
-
-    let promise3 = fetch(api3)
-    .then(res => (res.ok ? res : Promise.reject(res)))
-    .then(res => res.json())
-    .then(data => {
-      information.dreamTeam = data
-      return data
-    });
-
-    Promise.all([promise1, promise2, promise3])
+    // .then(() => {
+      //   fetch(`https://ancient-ocean-21689.herokuapp.com/https://fantasy.premierleague.com/api/dream-team/${nextGWID - 1}/`)
+      //   .then(res => (res.ok ? res : Promise.reject(res)))
+      //   .then(res => res.json())
+      //   .then(data => {
+        //     console.log('Promise 3')
+        //     information.dreamTeam = data
+        //     return data
+        //   });      
+        // }) 
+    Promise.all([promise1, promise2])
     .then(() => dispatch({
       type: 'FETCH_INFORMATION',
       payload: information
