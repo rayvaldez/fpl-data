6b77f2ac01ce6d42106e1a5399a710f904a37659
rayvaldez <ray_valdez@hotmail.co.uk>
2022-10-20 11:49:21 +0100

    Create function to find dream team gw and pass to dream team component

--- src/containers/LandingContainer.js
+++ src/containers/LandingContainer.js
@@ -6,7 +6,7 @@ import DreamTeam from '../components/DreamTeam';
 import CurrentGameweekFixtures from '../components/CurrentGameweekFixtures';
 import PreviousGameweekFixtures from '../components/PreviousGameweekFixtures';
 import Next5Fixtures from '../components/Next5Fixtures';
-import InForm from '../components/InForm';
+import InForm from '../components/InForm/InForm';
 
 import { fetchInformation } from '../actions/fetchInformation';
 import { connect } from 'react-redux';
@@ -16,15 +16,23 @@ class LandingContainer extends React.Component {
   componentDidMount() {
     this.props.fetchInformation()
   }
-  
+
   render() {
+
+    const findDreamTeamGW = () => {
+      if (this.props.information.general?.events[this.props.information.currentGW - 1].is_current === false) {
+        return this.props.information.general?.events[this.props.information.currentGW - 2]
+      } else {
+        return this.props.information.general?.events[this.props.information.currentGW - 1]
+      }
+    }
     console.log(this.props)
     return (
       <Box sx={{ paddingBottom: '10vh' }}>
-        <DeadlineTimer />
+        <DeadlineTimer nextGW={this.props.information.general?.events[this.props.information.currentGW - 1]}/>
         <CurrentGameweekFixtures fixtures={this.props.information.fixtures} currentGW={this.props.information.currentGW} />
-        <PreviousGameweekFixtures fixtures={this.props.information.fixtures} currentGW={this.props.information.currentGW} />
-        <DreamTeam currentGW={this.props.information.currentGW} />
+        <DreamTeam currentGW={findDreamTeamGW()} />
+        <PreviousGameweekFixtures fixtures={this.props.information.fixtures} previousGW={this.props.information.previousGW} />
         <Next5Fixtures fixtures={this.props.information.fixtures} currentGW={this.props.information.currentGW} />
         <InForm fixtures={this.props.information.fixtures} nextGW={this.props.information.currentGW} />
         <LandingInformation />
