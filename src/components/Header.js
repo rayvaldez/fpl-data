7bd6babb760607ee326e63d6be8902fe2fcec333
rayvaldez <ray_valdez@hotmail.co.uk>
2022-10-22 22:50:00 +0100

    Reformat header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Grid from '@mui/material/Grid';
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import { ThemeProvider } from '@mui/material/styles';
@@ -14,38 +13,34 @@ const Header = () => {
   theme = responsiveFontSizes(theme);
 
   return (
-    <AppBar position="static" sx={{
-      backgroundColor: '#151515',
-      width: '100%'
-    }}>
-      <Toolbar disableGutters>          
-        <ThemeProvider theme={theme}>
-          <Grid justify="space-between" container spacing={12}>
-            <Grid item xs={6}>                
-              <Typography variant="h4" sx={{
-                fontFamily: 'Masque',
-                fontSize: '1.5rem',
-                color: '#FAF9F6',
-                mt: '0.4em', 
-                ml: '0.8em'
-              }}>FPL</Typography>
-              <Typography variant="h4" sx={{
-                fontFamily: 'Masque',
-                fontSize: '.95rem',
-                color: '#FAF9F6',
-                mt: '-0.57em',
-                ml: '1.1em'
-              }}>Rank</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Box sx={{ mt: '-0.5em', mb: '-0.8em'}}>
-                <img src={PremierLeague} alt='logo' width="110" height="75" />
-              </Box>                
-            </Grid>
-          </Grid>
-        </ThemeProvider>
-      </Toolbar>
-    </AppBar>
+    <Box>
+      <AppBar position="static" sx={{
+        backgroundColor: '#151515',
+        width: '100%'
+      }}>
+        <Toolbar disableGutters>          
+          <ThemeProvider theme={theme}>           
+            <Typography variant="h4" sx={{
+              fontFamily: 'Masque',
+              fontSize: '1.5rem',
+              color: '#FAF9F6',
+              mt: '-0.2em', 
+              ml: '0.8em'
+            }}>FPL</Typography>
+            <Typography variant="h4" sx={{
+              fontFamily: 'Masque',
+              fontSize: '.95rem',
+              color: '#FAF9F6',
+              mt: '1.8em',
+              ml: '-3.51em'
+            }}>Rank</Typography>
+            <Box sx={{ mt: '0.6em', mr: '0.5em', position: 'absolute', right: '0'}}>
+              <img src={PremierLeague} alt='logo' width="110" height="75" />
+            </Box>                
+          </ThemeProvider>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
