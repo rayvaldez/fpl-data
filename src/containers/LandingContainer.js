91ae4ba418d7994d18fb9a21f887531803127d70
rayvaldez <ray_valdez@hotmail.co.uk>
2022-11-05 15:38:17 +0000

    Add MUI Grid for responsive components

--- src/containers/LandingContainer.js
+++ src/containers/LandingContainer.js
@@ -7,6 +7,7 @@ import CurrentGameweekFixtures from '../components/CurrentGameweekFixtures';
 import PreviousGameweekFixtures from '../components/PreviousGameweekFixtures';
 import Next5Fixtures from '../components/Next5Fixtures';
 import InForm from '../components/InForm/InForm';
+import { Grid } from '@material-ui/core';
 
 import { fetchInformation } from '../actions/fetchInformation';
 import { connect } from 'react-redux';
@@ -26,17 +27,17 @@ class LandingContainer extends React.Component {
         return this.props.information.general?.events[this.props.information.currentGW - 1]
       }
     }
-    console.log(this.props)
+    
     return (
-      <Box sx={{ paddingBottom: '10vh' }}>
-        <DeadlineTimer nextGW={this.props.information.general?.events[this.props.information.currentGW - 1]}/>
-        <CurrentGameweekFixtures fixtures={this.props.information.fixtures} currentGW={this.props.information.currentGW} />
-        <DreamTeam currentGW={findDreamTeamGW()} />
-        <PreviousGameweekFixtures fixtures={this.props.information.fixtures} previousGW={this.props.information.previousGW} />
-        <Next5Fixtures fixtures={this.props.information.fixtures} currentGW={this.props.information.currentGW} />
-        <InForm fixtures={this.props.information.fixtures} nextGW={this.props.information.currentGW} />
-        <LandingInformation />
-      </Box>
+      <Grid container lg={12} item>
+        <Grid item lg={12} md={12} sm={12} xs={12}><DeadlineTimer nextGW={this.props.information.general?.events[this.props.information.currentGW - 1]}/></Grid>
+        <Grid item xl={12} md={12} lg={6} sm={6} xs={12}><CurrentGameweekFixtures fixtures={this.props.information.fixtures} currentGW={this.props.information.currentGW} /></Grid>
+        <Grid item xl={6} md={6} lg={6} sm={6} xs={12}><PreviousGameweekFixtures fixtures={this.props.information.fixtures} previousGW={this.props.information.previousGW} /></Grid>
+        <Grid item xl={6} md={6} lg={6} sm={12} xs={12}><DreamTeam currentGW={findDreamTeamGW()} /></Grid>
+        <Grid item xl={6} md={6} lg={12} sm={12} xs={12}><Next5Fixtures fixtures={this.props.information.fixtures} currentGW={this.props.information.currentGW} /></Grid>
+        <Grid item xl={6} md={6} lg={12} sm={12} xs={12}><InForm fixtures={this.props.information.fixtures} nextGW={this.props.information.currentGW} /></Grid>
+        <Grid item xl={6} md={12} lg={12} sm={12} xs={12}><LandingInformation /></Grid> 
+      </Grid>
     )
   }
 }
